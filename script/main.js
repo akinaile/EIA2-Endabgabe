1b1dd2ee8dab37e76e411b7d0704430845dd9bd1
akinaile <aileen-akin@web.de>
2022-07-08 18:30:10 +0200

    Felder und Progessbars erstellt, 08.07

--- script/main.js
+++ script/main.js
@@ -5,10 +5,6 @@ var garden;
     function hndLoad() {
         let start = document.getElementById("start");
         start.addEventListener("click", hideScreen1);
-        //img click 
-        /*let img: HTMLImageElement = <HTMLImageElement>document.getElementById("img");
-        img.addEventListener("click", hideScreen1);
-        console.log("click");*/
     }
     // hide screen 1 and show screen 2
     function hideScreen1() {
@@ -16,6 +12,25 @@ var garden;
         let screen2 = document.getElementById("Screen2");
         screen1.style.display = "none";
         screen2.style.display = "unset";
+        createFields();
+    }
+    // create fields
+    function createFields() {
+        //iterate through rows
+        for (let index = 0; index < 7; index++) { //zählt die felder bis 7, selektiert die reihen 
+            document.querySelector("row" + index);
+            let rows = document.getElementById("row" + index);
+            //erstellt einzelne felder in reihen
+            for (let index = 0; index < 9; index++) { // erstellt 7 Reihen
+                let field = document.createElement("div");
+                field.classList.add("field");
+                rows.appendChild(field);
+                //erstellt für die fields jeweils eine progessbar, die den state abbilden
+                let stateScreen = document.createElement("div");
+                stateScreen.classList.add("state");
+                field.appendChild(stateScreen);
+            }
+        }
     }
 })(garden || (garden = {})); // namespace klammer
 //# sourceMappingURL=main.js.map
--- script/main.ts
+++ script/main.ts
@@ -3,10 +3,6 @@ namespace garden {
     function hndLoad(): void {
         let start: HTMLButtonElement = <HTMLButtonElement>document.getElementById("start");
         start.addEventListener("click", hideScreen1);
-        //img click 
-        /*let img: HTMLImageElement = <HTMLImageElement>document.getElementById("img");
-        img.addEventListener("click", hideScreen1);
-        console.log("click");*/
     }
 
 // hide screen 1 and show screen 2
@@ -15,7 +11,29 @@ namespace garden {
         let screen2: HTMLDivElement = <HTMLDivElement>document.getElementById("Screen2");
         screen1.style.display = "none";
         screen2.style.display = "unset";
+        createFields();
     }
 
+        // create fields
+    function createFields(): void { 
+
+            //iterate through rows
+        for (let index: number = 0; index < 7; index++) { //zählt die felder bis 7, selektiert die reihen 
+            document.querySelector("row" + index);
+            let rows: HTMLDivElement = <HTMLDivElement>document.getElementById("row" + index);
+    
+    
+            //erstellt einzelne felder in reihen
+            for (let index: number = 0; index < 9; index++) { // erstellt 7 Reihen
+                let field: HTMLDivElement = <HTMLDivElement>document.createElement("div");
+                field.classList.add("field");
+                rows.appendChild(field);
+                //erstellt für die fields jeweils eine progessbar, die den state abbilden
+                let stateScreen: HTMLDivElement = <HTMLDivElement>document.createElement("div");
+                stateScreen.classList.add("state");
+                field.appendChild(stateScreen);
+                }
+            }
+        }
 
 } // namespace klammer
